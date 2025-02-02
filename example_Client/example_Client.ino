ab63c882b824aade1f6ef3e5c5ceffb06863d2ec
augustobor <augusborromeo@gmail.com>
2025-02-02 17:24:50 -0300

    feat: split code into functions for scheduling

--- example_Client/example_Client.ino
+++ example_Client/example_Client.ino
@@ -1,26 +1,41 @@
 #include <WiFi.h>
 
-  // WiFi
-char wifi_ssid[32] = "Lucky";   // wifi_ssid
-char wifi_password[32] = "123412341234";    // wifi_password
-  // Server
-char server_ip[16] = "192.168.1.65";
-int server_port = 12345;
-
+// Definiciones de pines
 #define PIN_LED 2
 #define PIN_READ 0
 
-//constructor
+// Configuración de WiFi
+char wifi_ssid[32] = "Lucky";   // SSID de la red WiFi
+char wifi_password[32] = "123412341234";    // Contraseña de la red WiFi
+
+// Configuración del servidor
+char server_ip[16] = "192.168.1.65"; // IP del servidor
+int server_port = 12345; // Puerto del servidor
+
+// Definición del tamaño del buffer circular. Esto se hace para evitar problemas de concurrencia y bloqueos entre las tareas definidas
+#define BUFFER_SIZE 10
+uint16_t buffer[BUFFER_SIZE];
+int readIndex = 0; // Índice de lectura del buffer
+int writeIndex = 0; // Índice de escritura del buffer
+
+// Cliente WiFi
 WiFiClient client;
 
-void setup() {
+// Mutex para proteger el acceso al buffer
+SemaphoreHandle_t xMutex;
+
+// Funciones
+void analog_read_task(void *);
+void send_data_task(void *);
+
+void init() {
   Serial.begin(115200); delay(10);
   Serial.println();
   Serial.println("Starting...");
 
   pinMode(PIN_LED, OUTPUT);
 
-    // Conectar a WiFi
+  // Conectar a WiFi
   WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -28,28 +43,70 @@ void setup() {
   }
   Serial.println("\n \t - Conectado a WiFi");
 
-  for(int i=0;i<15;i++){ // señal visual de inicio completo
-    digitalWrite(PIN_LED, HIGH);delay(35);
+  // Señal visual de inicio completo
+  for(int i=0;i<15;i++){
+    digitalWrite(PIN_LED, HIGH); delay(35);
     digitalWrite(PIN_LED, LOW); delay(35);
   }
-
 }
 
+// Tarea para leer valores analógicos
+void analog_read_task(void * param) {
+  for (;;) {
+    uint16_t localMedicion = analogRead(PIN_READ); // Leer valor analógico
+    if (xSemaphoreTake(xMutex, portMAX_DELAY)) { // Tomar el mutex (semáforo que protege el acceso al buffer)
+      buffer[writeIndex] = localMedicion; // Escribir valor en el buffer
+      writeIndex = (writeIndex + 1) % BUFFER_SIZE; // Actualizar índice de escritura
+      xSemaphoreGive(xMutex); // Liberar el mutex
+    }
+    Serial.print("medicion : ");
+    Serial.println(localMedicion);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+// Tarea para enviar datos al servidor
+void send_data_task(void * param) {
+  for (;;) {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY)) {
+      if (readIndex != writeIndex) { // Verificar si hay datos para enviar
+        if (client.connect(server_ip, server_port)) { // Conectar al servidor
+          client.print(buffer[readIndex]);
+          readIndex = (readIndex + 1) % BUFFER_SIZE;
+        } else {
+          Serial.println("/ fallo envio de datos");
+        }
+      }
+      xSemaphoreGive(xMutex);
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+// Configuración inicial
+void setup() {
+  init();
+  xMutex = xSemaphoreCreateMutex();
+  xTaskCreatePinnedToCore(
+    analog_read_task,  
+    "AnalogReadTask",
+    10000,           
+    NULL,           
+    1,              
+    NULL,            
+    0);              
+
+  xTaskCreatePinnedToCore(
+    send_data_task,   
+    "SendDataTask", 
+    10000,          
+    NULL,            
+    1,              
+    NULL,           
+    1);             
+}
+
+// Bucle principal vacío ya que las tareas se ejecutan en paralelo
 void loop() {
-  uint16_t medicion = analogRead(PIN_READ); // lectura potenciometro
-  Serial.print("medicion : ");// envia valores por puerto serie
-  Serial.println(medicion);
-
-  if (client.connect(server_ip, server_port)) { // envia valores a servidor
-    client.print(medicion);
-  }else{
-    Serial.println("/ fallo envio de datos");
-  }
-  
-  for(int i=0;i<15;i++){ // señal visual de inicio completo
-  digitalWrite(PIN_LED, HIGH);delay(35);
-  digitalWrite(PIN_LED, LOW); delay(35);
-  }
-
-  delay(1000);
+  // El loop se deja vacío ya que las tareas se ejecutan en paralelo
 }
